788d58b3a42c4596ddcd77f223fb4a6783dcae34
Alessandro Sciarrillo <alessandr.sciarrill2@studio.unibo.it>
2022-06-03 14:53:22 +0200

    Soluzione Zeri

--- Scheletri/1-SoluzioneZeri/bisezione.m
+++ Scheletri/1-SoluzioneZeri/bisezione.m
@@ -2,23 +2,25 @@ function [x,xk,it]=bisez(fname,a,b,tol)
 
 fa=fname(a);
 fb=fname(b);
-if  
+if sign(fa)*sign(fb)>=0 %%
    error('intervallo non corretto');
 else
-    maxit= 
+    maxit=ceil(log2((b-a)/tol)-1);
     %fprintf('n. di passi necessari=%d \n',maxit);
     it=0;
     while it<=maxit && abs(b-a)>=tol
         it=it+1;
          %calcola xk(it) con la formula corrispondente
+        xk(it)=a+(b-a)*0.5; %%
         fxk=fname(xk(it));
         if fxk==0
             break;
         elseif sign(fxk)*sign(fa)>0
-            
-
+            a=xk(it); %%
+            fa=fxk; %%
         elseif sign(fxk)*sign(fb)>0
-            
+            b=xk(it); %%
+            fb=fxk; %%
         end
     end
     x=xk(it);
