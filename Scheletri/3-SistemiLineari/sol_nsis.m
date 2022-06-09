2bf601508ec2218faa055211168c71b9e6fa78c1
Alessandro Sciarrillo <alessandr.sciarrill2@studio.unibo.it>
2022-06-09 11:21:36 +0200

    Update sol_nsis.m

--- Scheletri/3-SistemiLineari/sol_nsis.m
+++ Scheletri/3-SistemiLineari/sol_nsis.m
@@ -26,3 +26,17 @@ else
     X=[];
     return
 end
+
+
+%%%%%%% OPPURE %%%%%%%%%
+function A1=inversa(A)
+
+I=eye(size(A));
+[L,U,P,flag]=LU_nopivot(A);
+A1=zeros(size(A));
+
+for i=1:n
+    b=I(:,i);
+    [x,flag]=LU_SOLVE(L,U,P,b);  %Se si una gaus con pivoting b ->  P*b ;
+    A1(:,i)=x;
+end
